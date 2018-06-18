edccde03647d93008e7aeb4c806d65fcc568e7b5
Timothy Suy <timothy.suy@gmail.com>
2018-06-18 16:20:25 +0200

    rudimentary authorization framework

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
-const io = require('socket.io')();
+const io = require('socket.io')()
 
-const initialCount = 1234;
+const initialCount = 1234
 
 io.on('connection', (client) => {
+  client.auth = false
+  client.on('authenticate', function (data) {
+    //check the auth data sent by the client
+    checkAuthToken(data.token, function (err, success) {
+      if (!err && success) {
+        console.log('Authenticated client ', client.id)
+        client.auth = true
+      }
+    })
+  })
+  
+  setTimeout(function () {
+    //If the client didn't authenticate, disconnect it
+    if (!client.auth) {
+      console.log('Disconnecting client ', client.id)
+      client.disconnect('unauthorized')
+    }
+  }, 1000)
+  
+  //emit random addition of 1-5 appointments
   let count = initialCount
-  console.log('connection');
   client.on('subscribeToAppointmentService', (interval) => {
     setInterval(() => {
       client.emit('appointmentCount', {
         'enterprise': 'some_enterprise',
         'count': count,
-      });
+      })
       count += Math.floor((Math.random() * 5) + 1)
-    }, interval);
-  });
-});
+    }, interval)
+  })
+})
 
-const port = 8000;
-io.listen(port);
-console.log('listening on port ', port);
+checkAuthToken = ((token, cb) => {
+  console.dir(token);
+  return cb(null, true);
+})
+
+const port = 8000
+io.listen(port)
+console.log('listening on port ', port)
