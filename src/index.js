456d4f2c71a1e570657216f404699cf54587277f
Timothy Suy <timothy.suy@gmail.com>
2018-06-18 11:51:43 +0200

    .gitignore, package.json

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const io = require('socket.io')();
+
+const initialCount = 1234;
+
+io.on('connection', (client) => {
+  let count = initialCount
+  console.log('connection');
+  client.on('subscribeToAppointmentService', (interval) => {
+    setInterval(() => {
+      client.emit('appointmentCount', {
+        'enterprise': 'some_enterprise',
+        'count': count,
+      });
+      count += Math.floor((Math.random() * 5) + 1)
+    }, interval);
+  });
+});
+
+const port = 8000;
+io.listen(port);
+console.log('listening on port ', port);
