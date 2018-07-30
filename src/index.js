5b4066457853a5ab767595499d546476f6752bfe
Timothy Suy <timothy.suy@gmail.com>
2018-07-30 14:52:48 +0200

    don't let client decide interval, also listen for GET on '/'

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
-const initialCount = 1234
+app.get('/', function(req, res){
+  res.sendFile(path.resolve(__dirname + '/../public/index.html'));
+});
 
 io.origins('*:*')
 io.on('connection', (client) => {
   //emit random addition of 1-5 appointments
-  let count = initialCount
-  client.on('subscribeToAppointmentService', (interval) => {
+  let count = 2500
+  client.on('subscribeToAppointmentService', () => {
     setInterval(() => {
       client.emit('appointmentCount', {
         'enterprise': 'some_enterprise',
         'count': count,
       })
       count += Math.floor((Math.random() * 5) + 1)
-    }, interval)
+    }, Math.floor((Math.random() * 2000) + 500))
   })
 })
 
